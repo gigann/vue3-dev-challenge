373c0734324d82deaa8d70f49d0f14e41f6b4bd0
gigann <issacgann@gmail.com>
2025-05-06 15:04:04 +0200

    implemented bookmarks

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,28 +3,31 @@ import { computed, ref } from "vue";
 import "material-symbols";
 import { store } from "../store/store";
 
-
 const props = defineProps({
   title: String,
   category: String,
-  isBookmarked: Boolean,
+  isBookmarked: Boolean, // by default
   img: String,
 });
+
+const isBookmarked = ref(props.isBookmarked);
 
 const imageURL = computed(
   () =>
     `url(${new URL(`../assets/images/${props.img}.jpg`, import.meta.url).href})`
 );
+
 </script>
 
 <template>
   <div
-  v-if="store.isFiltered(props.category) || store.filters.length === 0"
+  v-if="isBookmarked || store.isFiltered(props.category) || store.filters.length === 0"
   class="card"
   :style="{ backgroundImage: imageURL}"
   >
     <div class="card-buttons">
-      <button class="bookmark-button"><span class="material-symbols-outlined">bookmark</span></button>
+      <button class="bookmarked-button" v-if="isBookmarked" @click="isBookmarked = !isBookmarked"><span class="material-symbols-outlined" >bookmark</span></button>
+      <button class="not-bookmarked-button" v-if="!isBookmarked" @click="isBookmarked = !isBookmarked"><span class="material-symbols-outlined">bookmark</span></button>
       <button class="menu-button"><span class="material-symbols-outlined">menu</span></button>
     </div>
 
